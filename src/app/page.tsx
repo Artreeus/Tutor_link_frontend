a2ab6963a39cb706a06271848dd51fbf748f0df4
Artreeus <142108262+Artreeus@users.noreply.github.com>
2025-03-05 02:45:33 +0600

    project structure created

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-red-900">Welcome To NextMart Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
